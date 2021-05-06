18edef5c20615dbb8faf4e25c8f4cdaae58b4eec
João Moraes <joaoadm94@gmail.com>
2021-05-06 15:31:14 -0300

    atribui quantidade de moradores por apto corretamente

--- README.md
+++ README.md
@@ -1,4 +1,60 @@
-# PC_Elevadores
+# Programacao Concorrente - Projeto 1
 
-#### Projeto 1 
+### Objetivo
 
+O presente projeto tem como objetivo o desenvolvimento de algoritmos para tratar problemas de comunicação entre processos através de uma memória compartilhada.
+
+### Proposta: modelagem de um sistema de elevadores de um prédio
+
+Considere um edificio de X andares e Y apartamentos por andar.
+
+O edifício conta com dois elevadores: um para uso social e outro para uso de serviço.
+
+#### Uso normal do elevador
+
+Os moradores do edifício podem usar qualquer elevador. 
+
+Cada elevador tem espaço para quatro pessoas.
+
+Após um certo número de usos, o elevador deve ser limpo pelo zelador, e para isso o elevador deve ficar bloqueado no térreo.
+
+No caso de vários zeladores, eles não podem limpar todos os elevadores ao mesmo tempo. Uma certa quantidade de elevadores deve ficar ativa para não prejudicar o fluxo de pessoas.
+
+#### Uso em contexto de pandemia
+
+Devido às restrições do coronavírus, há novas regras para os elevadores.
+
+Um morador só pode entrar no elevador se ele estiver: 1) vazio ou 2) ocupado por pessoas do mesmo apartamento.
+
+### Ciclo de vida de um morador
+
+O morador passa boa parte do seu tempo em casa, mas as vezes precisa sair.
+
+Para sair, ele deve chamar o elevador.
+
+Para o morador embarcar, o elevador deve:
+ * parar no andar do morador e
+ * ter espaço disponivel e
+ * estar indo para o mesmo sentido
+
+Ao embarcar, o morador aguarda até o elevador chegar ao andar desejado.
+
+O morador desce e realiza suas atividades. 
+
+Eventualmente, ele precisa voltar para casa. 
+
+Para voltar, ele deve chamar o elevador.
+
+As regras para o embarque se aplicam aqui novamente.
+
+Quando o morador chega em casa, ele fica até precisar sair novamente.
+
+Quando o morador usa o elevador, ele pode perceber que o mesmo está sujo. Nesse caso, ele aperta o botão de manutenção, avisando ao zelador que o elevador precisa de limpeza.
+
+### Ciclo de vida do zelador
+
+O zelador realiza diversas atividades no edifício. A limpeza do elevador é uma entre elas.
+
+Quando o zelador percebe o sinal de manutenção do elevador, ele deve chamá-lo para limpeza no térreo.
+
+O zelador iniciará a limpeza quando o elevador chegar vazio ao térreo. 
--- elevadores.c
+++ elevadores.c
@@ -3,17 +3,17 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <semaphore.h>
+#include <time.h>
 
-#define ANDARES 5 // numero de andares
+#define ANDARES 3 // numero de andares
+#define APTOS 2 // numero de apartamentos por andar
 #define SOCIAL 1 // numero de elevadores sociais
 #define SERVICO 5  // numero de elevadores de servico
-#define FUNCIS 1 // numero de funcionarios
-#define MAX_MORADORES 5 // numero maximo de moradores
+#define ZELADOR 1 // numero de funcionarios
+#define MAX_MORADORES 5 // numero maximo de moradores por apto
 
-
-void * produtor(void * meuid);
-void * consumidor (void * meuid);
-
+// matriz que representa a quantidade de moradores por apartamento, e por andar
+int moradores[ANDARES][APTOS];
 
 void main(argc, argv)
 int argc;
@@ -23,14 +23,42 @@ char *argv[];
   int erro;
   int i, n, m;
   int *id;
+  int conta_moradores = 0;
+  int valor;
 
+  srand(time(NULL));
+
+  // populando a matriz de moradores
+  for (int j = 0; j < ANDARES; j++) {
+    for (int k = 0; k < APTOS; k++) {
+      valor = 0;
+      while (valor == 0) {
+        valor = rand() % MAX_MORADORES;
+      }
+      moradores[j][k] = valor;
+      conta_moradores += valor;
+    }
+  }
+
+  printf("Total de moradores: %d\n", conta_moradores);
+
+  // criando as threads, cada uma é um morador
+  for (int j = 0; j < ANDARES; j++) {
+    for (int k = 0; k < APTOS; k++) {
+      printf("Apto %d0%d: %d\n", j+1, k+1, moradores[j][k]);
+    }
+  }
+
+
+
+  /*
   pthread_t tid[PR];
    
   for (i = 0; i < PR; i++)
   {
     id = (int *) malloc(sizeof(int));
-    *id = i;
-    erro = pthread_create(&tid[i], NULL, produtor, (void *) (id));
+    *id = i; 
+    erro = pthread_create(&tid[i], NULL, morador, (void *) (id));
 
     if(erro)
     {
@@ -55,25 +83,46 @@ char *argv[];
   }
  
   pthread_join(tid[0],NULL);
-
+  */
 }
 
-void * produtor (void* pi)
+void * morador (void* pi)
 {
  
   while(1)
   {
-   
-
+    // fica em casa até decidir sair
+    // chama o elevador
+    // confere a possibilidade de entrar no elevador
+        // tem espaço? está indo para o mesmo sentido? 
+        // os ocupantes são do mesmo apartamento?
+        // (talvez só a questão do espaço seja relevante)
+    // espera chegar no andar desejado para sair
+    // se o elevador está sujo, sinaliza ao zelador para realizar a limpeza
   }
   pthread_exit(0);
   
 }
 
-void * consumidor (void* pi)
+void * elevador (void * pi) {
+  while (1) {
+    // aguarda chamado para algum andar
+    // quando recebe o chamado:
+      // fecha a porta
+      // vai até o andar desejado
+      // (precisa armazenar o sentido do elevador para enfileiramento de chamadas?)
+  }
+}
+
+void * funcionario (void* pi)
 {
  while(1)
   {
+    // realiza outras tarefas
+
+    // quando o elevador está sujo, prepara para entrar e limpar
+
+    // o elevador fica parado enquanto está sendo limpo
  
   }
   pthread_exit(0);
